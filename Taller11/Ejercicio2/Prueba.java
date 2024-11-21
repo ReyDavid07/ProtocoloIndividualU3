ac589847478124a05443a95c8722fe5c3004ad1e
ReyDavid07 <reydavidborrego@hotmail.com>
2024-11-21 12:27:57 -0500

    Update Prueba.java

--- Taller11/Ejercicio2/Prueba.java
+++ Taller11/Ejercicio2/Prueba.java
@@ -3,14 +3,14 @@ package Ejercicio2;
 public class Prueba {
     public static void main(String[] args) {
         
-        Empleado gerente = new Gerente("Ana", 3000, 1000); 
+        Empleado gerente = new Gerente("Enrique", 3000, 1000); 
         System.out.println("Detalles del Gerente:");
         gerente.mostrarDetalles();
 
         System.out.println();
 
         
-        Empleado vendedor = new Vendedor("Carlos", 2000, 500); 
+        Empleado vendedor = new Vendedor("Juan", 2000, 500); 
         System.out.println("Detalles del Vendedor:");
         vendedor.mostrarDetalles();
     }
